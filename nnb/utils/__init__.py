8547029af8c8c53dca66619ffd7605b15cfd1b2d
Frederico Tommasi Caroli <ftcaroli@gmail.com>
2015-10-15 20:57:40 -0300

    Closes #17

--- nnb/utils/__init__.py
+++ nnb/utils/__init__.py
@@ -1,4 +1,8 @@
-import plot_procedure
+try:
+    import matplotlib.pyplot as plt
+    import plot_procedure
+except ImportError:
+    pass
 import ptb
 from word_vecs import WordVecsHelper
 from options import Options
