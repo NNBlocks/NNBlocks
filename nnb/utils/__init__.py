043a6a30a8bde2bd48f69ce41686aefc3f628a2b
Frederico Tommasi Caroli <ftcaroli@gmail.com>
2015-10-15 21:00:27 -0300

    Fixing except too broad

--- nnb/utils/__init__.py
+++ nnb/utils/__init__.py
@@ -1,8 +1,11 @@
+matplot_imported = False
 try:
     import matplotlib.pyplot as plt
-    import plot_procedure
+    matplot_imported = True
 except ImportError:
     pass
+if matplot_imported:
+    import plot_procedure
 import ptb
 from word_vecs import WordVecsHelper
 from options import Options
