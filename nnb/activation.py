973dbe6e7dc6378d3b4034001075254fdd3dd6fa
Frederico Tommasi Caroli <ftcaroli@gmail.com>
2015-10-17 14:37:04 -0300

    leaky_ReLU added

--- nnb/activation.py
+++ nnb/activation.py
@@ -16,3 +16,10 @@ def threshold(t, yes=1., no=0.):
 
 def ReLU(a):
     return T.switch(T.lt(a, 0.), 0., a)
+
+def leaky_ReLU(alpha):
+    def r(a):
+        f1 = 0.5 * (a + alpha)
+        f2 = 0.5 * (a - alpha)
+        return f1 * a + f2 * abs(a)
+    return r
