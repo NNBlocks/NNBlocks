7cd0689df29b32027f79865caa4a5ea9a0fd1421
Frederico Tommasi Caroli <ftcaroli@gmail.com>
2015-10-18 22:16:24 -0200

    puts costs in a single file

--- nnb/cost.py
+++ nnb/cost.py
@@ -20,3 +20,15 @@ def cross_entropy(p, y):
 
 def cross_entropy_summed(p, y):
     return -T.sum((y * T.log(p)))
+
+def mean_square_error(p, y):
+    if p.ndim == 0 and y.ndim == 0:
+        return T.sqr(p - y) / 2
+    elif p.ndim == 1 and y.ndim == 1:
+        return T.mean(T.sqr(p - y)) / 2
+    elif p.ndim == 2 and y.ndim == 2:
+        return T.mean(T.sqr(p - y).sum(axis=1)) / 2
+    else:
+        error_str = "Invalid dimensions for mean square error: {0} and {1}."
+        error_str = error_str.format(p.ndim, y.ndim)
+        raise ValueError(error_str)
--- nnb/cost/__init__.py
+++
@@ -1,7 +0,0 @@
-from cross_entropy import (
-    cross_entropy,
-    cross_entropy_summed
-)
-from mean_square_error import (
-    mean_square_error
-)
