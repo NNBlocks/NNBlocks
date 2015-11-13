40a34ff80ec3f3893fb5b93760b9dea0405f9c9e
Frederico Tommasi Caroli <ftcaroli@gmail.com>
2015-11-13 00:48:17 -0200

    Adding license headers

--- nnb/utils/ptb/__init__.py
+++ nnb/utils/ptb/__init__.py
@@ -1,2 +1,19 @@
+# NNBlocks is a Deep Learning framework for computational linguistics.
+#
+#   Copyright (C) 2015 Frederico Tommasi Caroli
+#
+#   NNBlocks is free software: you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free
+#   Software Foundation, either version 3 of the License, or (at your option)
+#   any later version.
+#
+#   NNBlocks is distributed in the hope that it will be useful, but WITHOUT ANY
+#   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU General Public License along with
+#   NNBlocks. If not, see http://www.gnu.org/licenses/.
+
 from nnb.utils.ptb.ptb_tree import PTBTreeNode
 from nnb.utils.ptb.ptb_parser import PTBParser
