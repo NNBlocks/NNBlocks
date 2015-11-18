1d6a169511dd0d095b3120ff1480bd84978f3ace
Frederico Tommasi Caroli <ftcaroli@gmail.com>
2015-11-18 17:24:47 -0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NNBlocks",
+    version="0.1.a.dev",
+    install_requires = ['theano>=0.7.1', 'matplotlib'],
+    packages=find_packages(),
+    author="Frederico Tommasi Caroli",
+    author_email="ftcaroli@gmail.com",
+    description="NNBlocks is a Deep Learning framework made to build linguistics neural models",
+    license="GPL v3",
+    keywords="deep learning linguistics machine learning theano",
+    url="https://github.com/NNBlocks/NNBlocks"
+)
